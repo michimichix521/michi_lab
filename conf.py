f52e228b93ce767eec7bcea720e3c05884775c0a
michimichix521 <michimichix521@gmail.com>
2020-09-30 00:15:08 +0900

    改善

--- conf.py
+++ conf.py
@@ -33,7 +33,7 @@ author = 'Michihiro Tsukano'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark'
+    'recommonmark','sphinx.ext.githubpages'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
